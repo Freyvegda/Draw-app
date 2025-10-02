27caa4b94e85ce3905f61583bc0e332122917a75
Freyvegda <freyvegda@gmail.com>
2025-10-02 22:10:16 +0530

    basic backend complete

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -110,4 +110,19 @@ app.post('/room',middleware, async (req, res)=>{
     }
 })
 
+app.get('/chats/:roomId', async (req, res)=>{
+    const roomId = req.params.roomId;
+    const messages = await prismaClient.chat.findMany({
+        where:{
+            roomId: roomId
+        },
+        orderBy:{
+            id: "desc"
+        },
+        take: 50
+    });
+
+    res.json(messages)
+})
+
 app.listen(3002)
