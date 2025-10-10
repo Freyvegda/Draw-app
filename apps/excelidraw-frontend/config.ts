4488cc2f6f8ee8a7c4df61bf7f6fca12df5eeecc
Freyvegda <freyvegda@gmail.com>
2025-10-10 22:12:25 +0530

    ws deploy

--- apps/excelidraw-frontend/config.ts
+++ apps/excelidraw-frontend/config.ts
@@ -1,2 +1,2 @@
 export const HTTP_BACKEND = "https://draw-app-fgfu.onrender.com"
-export const WS_URL = "https://draw-app-ws-lyj6.onrender.com";
+export const WS_URL = "wss://draw-app-ws-lyj6.onrender.com";
